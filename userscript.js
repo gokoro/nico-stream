9a7059389b3ea13e45aa9a13cff6faf5e2049e7a
Lincroe <lincroe@gmail.com>
2025-01-07 16:58:46 +0900

    bump to 0.12

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nico-Stream
 // @namespace    http://tampermonkey.net/
-// @version      0.11
+// @version      0.12
 // @description  Get stream of nico for free
 // @author       You
 // @match        https://nicochannel.jp/*
